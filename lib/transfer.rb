af53610790dbdf23aeff9d20402379f4af11eacb
Delia Constantino <dconstan92@gmail.com>
2020-11-12 00:26:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       end
     else
       @status = "rejected"
-     "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     end
    end
 end
